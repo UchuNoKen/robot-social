c8a801c15ad2d73ac15113ff4e91216476ed23dd
UchuNoKen <xen.o.fite@gmail.com>
2019-02-09 15:41:11 -0500

    Created store and Provider to pass store down to App.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { Provider, connect } from "react-redux";
 import { createStore } from "redux";
 import "./index.css";
 import App from "./containers/App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 import { searchRobots } from "./reducers";
 import "tachyons";
 
 const store = createStore(searchRobots);
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+// registerServiceWorker();
